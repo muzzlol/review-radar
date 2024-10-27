8407fd4182eaa65bb002d81750c0682b7f388ead
muzzlol <muzxmmilkhxn@gmail.com>
2024-10-27 10:13:38 +0530

    header

---
+++ frontend/src/components/header.tsx
@@ -0,0 +1,14 @@
+import { FC } from 'react'
+
+export const Header: FC = () => {
+  return (
+    <header className="w-full py-6 bg-gradient-to-r from-teal-500 to-blue-500">
+      <h1 className="text-center text-6xl md:text-7xl lg:text-8xl font-extrabold text-white tracking-tight font-['Kaushan_Script']">
+        Review
+        <span className="bg-clip-text text-transparent bg-gradient-to-r from-purple-400 to-pink-600">
+          Radar
+        </span>
+      </h1>
+    </header>
+  )
+}
