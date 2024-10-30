faf26372577a2e09d869afd0653b595eed8a6f32
muzzlol <muzxmmilkhxn@gmail.com>
2024-10-30 23:42:28 +0530

    cleanup

--- backend/app/config.py
+++ backend/app/config.py
@@ -2,7 +2,7 @@ from pydantic import BaseSettings
 
 class Settings(BaseSettings):
     openai_api_key: str
-    fake_review_model_path: str = "models/fake_review_model.pkl"
+    fake_review_model_path: str = "models/fake_review_model.joblib"
 
     class Config:
         env_file = ".env"  # Loads variables from the .env file
